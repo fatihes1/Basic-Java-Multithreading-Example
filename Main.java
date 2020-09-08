0978a7be9906114ec5d21a3c0e0e3bdc82b71fa6
Fatih Es <54971670+fatihes1@users.noreply.github.com>
2020-09-08 19:21:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Fatih ES \n18360859021 \n------------------------------------");
+        MultiThreadExample multiThreadExample = new MultiThreadExample();
+        multiThreadExample.start();
+        InputAl inputAl = new InputAl();
+        inputAl.start();
+    }
+}
+ class MultiThreadExample extends Thread {
+     public static char harf ;
+     MultiThreadExample() {
+     }
+    public void run () {
+        while(harf != 'q') {
+            Random random = new Random();
+            int sayi = random.nextInt();
+            System.out.println(sayi);
+            try {
+                sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class InputAl extends Thread {
+    MultiThreadExample multiThreadExample = new MultiThreadExample();
+    InputAl () {
+    }
+    public void run () {
+
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            MultiThreadExample.harf = scanner.next().charAt(0);
+            if(!multiThreadExample.isAlive()){
+                break;
+            }
+        }
+        }
+    }
+
